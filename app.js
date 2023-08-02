e6caddfa2668cd011f682b18e5e11f0b277f918b
Hayden Bradfield <hbradfield20@gmail.com>
2023-08-02 12:45:43 -0400

    configure nunjucks

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const config = require("./config");
 const morganMiddleware = require("./config/morgan");
 const logger = require("./config/logger");
+const nunjucks = require("nunjucks");
 
 const app = express();
+
+nunjucks.configure('views', {
+    express: app
+});
 
 app.use(morganMiddleware);
 
