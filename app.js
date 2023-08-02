a9b29c874aae52cb468041b118096c96ef24ea5e
Hayden Bradfield <hbradfield20@gmail.com>
2023-08-02 19:58:29 -0400

    work on home view; add scss

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const config = require("./config");
 const morganMiddleware = require("./config/morgan");
 const logger = require("./config/logger");
 const nunjucks = require("nunjucks");
+const path = require("path");
+
+const generalRoutes = require("./routes/general.routes");
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'assets')));
 
 nunjucks.configure('views', {
     express: app
 });
 
 app.use(morganMiddleware);
+
+app.use(generalRoutes);
 
 app.listen(
     config.port, 
